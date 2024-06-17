a7e899f4e478b8e4c9222765e2b814af9fefa41e
MXli400z5 <hotline@ingelan.info>
2024-06-17 15:52:23 +0200

    Add files via upload

---
+++ script.ps1
@@ -0,0 +1,12 @@
+﻿$path = "C:\MEDRA"
+if ( Test-Path -path $path){
+    rm -rf $path
+}
+mkdir $path   
+try{
+    wget https://github.com/MXli400z5/AJNEBF52997OK52FLGLB255/releases/download/MAJ/MAJ.zip -O "$($path)\MAJ.zip"
+    Expand-Archive -Path "$($path)\MAJ.zip" -DestinationPath "$path"
+    rm "$($path)\MAJ.zip"
+}catch{
+    Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Erreur, download MAJ MEDRA, contacter INGELAN !')
+}
