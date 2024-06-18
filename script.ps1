1b1d64f2ab64dca5fc9afa1dbfa092ac0fecf11a
MXli400z5 <hotline@ingelan.info>
2024-06-18 12:25:03 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,13 +1,22 @@
 $path = "C:\MEDRA"
-if ( Test-Path -path $path){
-    rm -rf $path
+
+# Vérifie si le chemin existe
+if (Test-Path -Path $path) {
+    # Supprime de manière récursive
+    Remove-Item -Path $path -Recurse -Force
 }
-mkdir $path   
-try{
-    wget https://github.com/MXli400z5/AJNEBF52997OK52FLGLB255/releases/download/MAJ/MAJ.zip -O "$($path)\MAJ.zip"
+
+New-Item -Path $path -ItemType Directory   
+try {
+    Invoke-WebRequest -Uri "https://github.com/MXli400z5/AJNEBF52997OK52FLGLB255/releases/download/MAJ/MAJ.zip" -OutFile "$($path)\MAJ.zip"
+
     Expand-Archive -Path "$($path)\MAJ.zip" -DestinationPath "$path"
-    rm "$($path)\MAJ.zip"
-}catch{
-    Add-Type -AssemblyName PresentationFramework; [System.Windows.MessageBox]::Show('Erreur, download MAJ MEDRA, contacter INGELAN !')
+
+    Remove-Item -Path "$($path)\MAJ.zip" -Force
+} catch {
+    Add-Type -AssemblyName PresentationFramework
+    [System.Windows.MessageBox]::Show('Erreur, téléchargement de MAJ MEDRA, contacter INGELAN !')
 }
+
+# Définit la politique d'exécution des scripts
 Set-ExecutionPolicy RemoteSigned -Force
