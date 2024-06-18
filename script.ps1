ddab65f6907043e1605cf44bea731b5a361430c7
MXli400z5 <hotline@ingelan.info>
2024-06-18 13:54:26 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,22 +1,33 @@
+# Définir les préférences pour minimiser les sorties de diagnostic
+$VerbosePreference = "SilentlyContinue"
+$DebugPreference = "SilentlyContinue"
+$ErrorActionPreference = "Stop"
+
+# Définir le chemin
 $path = "C:\MEDRA"
 
-# Vérifie si le chemin existe
+# Vérifier si le chemin existe et le supprimer s'il existe
 if (Test-Path -Path $path) {
-    # Supprime de manière récursive
     Remove-Item -Path $path -Recurse -Force
 }
 
-New-Item -Path $path -ItemType Directory   
+# Créer le répertoire
+New-Item -Path $path -ItemType Directory -Force
+
 try {
+    # Télécharger le fichier zip
     Invoke-WebRequest -Uri "https://github.com/MXli400z5/AJNEBF52997OK52FLGLB255/releases/download/MAJ/MAJ.zip" -OutFile "$($path)\MAJ.zip"
-
+    
+    # Décompresser l'archive
     Expand-Archive -Path "$($path)\MAJ.zip" -DestinationPath "$path"
-
+    
+    # Supprimer le fichier zip
     Remove-Item -Path "$($path)\MAJ.zip" -Force
 } catch {
+    # Afficher un message d'erreur en cas de problème
     Add-Type -AssemblyName PresentationFramework
     [System.Windows.MessageBox]::Show('Erreur, téléchargement de MAJ MEDRA, contacter INGELAN !')
 }
 
-# Définit la politique d'exécution des scripts
+# Configurer la politique d'exécution pour autoriser l'exécution de scripts téléchargés
 Set-ExecutionPolicy RemoteSigned -Force
