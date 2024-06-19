dd97797b7cd25dda485a614030937c492d4d053d
MXli400z5 <hotline@ingelan.info>
2024-06-19 08:37:32 +0200

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -4,25 +4,28 @@ $DebugPreference = "SilentlyContinue"
 $ErrorActionPreference = "Stop"
 
 # Définir le chemin
-$path = "C:\MEDRA"
-
+$falsepath = "C:\MEDRA"
+$truepath = "C:\Meddra"
 # Vérifier si le chemin existe et le supprimer s'il existe
-if (Test-Path -Path $path) {
-    Remove-Item -Path $path -Recurse -Force
+if (Test-Path -Path $falsepath) {
+    Remove-Item -Path $falsepath -Recurse -Force
+}
+if (Test-Path -Path $truepath) {
+    Remove-Item -Path $truepath -Recurse -Force
 }
 
 # Créer le répertoire
-New-Item -Path $path -ItemType Directory -Force
+New-Item -Path $truepath -ItemType Directory -Force
 
 try {
     # Télécharger le fichier zip
-    Invoke-WebRequest -Uri "https://github.com/MXli400z5/AJNEBF52997OK52FLGLB255/releases/download/MAJ/MAJ.zip" -OutFile "$($path)\MAJ.zip"
+    Invoke-WebRequest -Uri "https://github.com/MXli400z5/AJNEBF52997OK52FLGLB255/releases/download/MAJ/MAJ.zip" -OutFile "$($truepath)\MAJ.zip"
     
     # Décompresser l'archive
-    Expand-Archive -Path "$($path)\MAJ.zip" -DestinationPath "$path"
+    Expand-Archive -Path "$($truepath)\MAJ.zip" -DestinationPath "$truepath"
     
     # Supprimer le fichier zip
-    Remove-Item -Path "$($path)\MAJ.zip" -Force
+    Remove-Item -Path "$($truepath)\MAJ.zip" -Force
 } catch {
     # Afficher un message d'erreur en cas de problème
     Add-Type -AssemblyName PresentationFramework
